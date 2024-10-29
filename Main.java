8d9492c67b5adc7b8acbf1c357aa888a961b1169
cyborgWyrm <esorRevurg@gmail.com>
2024-10-29 14:11:58 -0600

    Pseudocoding

--- Main.java
+++ Main.java
@@ -26,7 +26,29 @@ public class Main
 	public static void main(String[] args) 
 	{
 		
-		System.out.println();
+		/*
+		For part 1 you need to create both of the above classes.
+		1.	In the main method of Main, instantiate four ThreadOperation objects, start them, and join them. Each ThreadOperation will take as input (through the constructor) two matrices and a quadrant indicator. The indicator could be a String, an int, an enum or a set of indexes. It’s up to you.
+
+		2.	In Main.java, write a static method named print2dArray that takes a two-dimensional array as input and prints it out with the rows and columns lined up. You must use System.out.printf.
+
+		3.	Instantiate a test 2d array with any values you like in main and use it to verify that print2dArray works.
+
+		4.	The filename should be given through the command prompt and passed into main via String[] args
+
+		5.	Open and connect to the file using a Scanner.
+
+		6.	Read in the number of rows and columns and save these in local variables in main.
+
+		7.	Read in the first and second matrices (two-dimensional arrays) from the file. I recommend writing a method to accomplish this task and calling the method twice (once for each matrix). Consider using this method header: 
+
+
+		public static int[][] matrixFromFile(int rows, int columns, Scanner file_reader)
+
+		NOTE: if you are using a static scanner or an object-oriented approach then you may not need to pass these arguments to the method.
+
+
+		*/
 		
 		
 		
--- ThreadOperation.java
+++ ThreadOperation.java
@@ -1,2 +1,22 @@
 
 
+public class ThreadOperation extends Thread {
+	
+	// Quadrant is an enum that refers to which quadrant is being modified by this thread
+	
+	// instance variables
+	private Quadrant quadrant;
+	private int[][] matrix1;
+	private int[][] matrix2;
+	// private int[][] resultMatrix?
+	
+	// constructor
+	public ThreadOperation(int[][] matrix1, int[][] matrix2, Quadrant quadrant) {
+		
+	}
+	
+	
+	
+	
+	
+}
